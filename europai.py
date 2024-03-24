60d80bdb0ae0310d3ade67e9b281fe4f40256e89
Timothée de Almeida <burnedsyn@gmail.com>
2024-03-24 09:03:37 +0000

    mise a jour test

--- europai.py
+++ europai.py
@@ -1,10 +1,22 @@
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
 import streamlit as st
-from langchain.memory import ConversationBufferMemory
-from langchain_community.callbacks import StreamlitCallbackHandler
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from langchain_groq import ChatGroq
 import os
-import json
+import yaml
+
+api_key = st.secrets["MISTRAL_API_KEY"]
+
+model = "mistral-small-latest"
+# Function to reset the state
+def reset_state():
+    for key in st.session_state:
+        del st.session_state[key]
+
+def load_config(config_path):
+        with open(config_path, 'r') as file:
+            return yaml.safe_load(file)
+
+
 
 def main():
     st.set_page_config(
@@ -21,102 +33,85 @@ def main():
     <br>Bien entendu,comme toutes les intelligences artificielles actuellement sur le marché, Europai peut faire des erreurs. 
     Envisagez de vérifier les informations importantes.
     """
+    
 
     st.markdown(multiline_text, unsafe_allow_html=True)
+    client = MistralClient(api_key=api_key)
+
+    # Initialize the model in session state if it's not already set
+    if "mistral_model" not in st.session_state:
+        st.session_state["mistral_model"] = "mistral-small-latest"
+
     
-        
-   
-
-    if 'chat_message' not in st.session_state:
-     st.session_state['chat_history']=[]
-    test=StreamlitChatMessageHistory()
-    msgs = test
-   
-    if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
-        msgs.clear()
-        msgs.add_ai_message("comment puis je vous aider?")
-        st.session_state.chat_history = {}
-
+    #definition avatars chat
     avatars = {"human": "📩", "user": "📩","ai": "🇪🇺",'assistant':"🇪🇺"}
-    for idx, msg in enumerate(msgs.messages):
-        with st.chat_message(avatars[msg.type]):
-            st.markdown(msg.content)
-
-    if prompt := st.chat_input(placeholder="Quel est votre question concernant les elections 2024?"):
-        st.chat_message(avatars["user"]).markdown(prompt)
-        msgs.add_user_message(prompt)
-        model = 'mixtral-8x7b-32768'
-
-        llm = ChatGroq(
-                temperature=0,
-                groq_api_key=st.secrets["GROQ_API_KEY"],
-                model_name=model,
-                streaming=True,
-                
-            )
+    config_path='config/config.yaml'
+    config=load_config(config_path)
+   # Add system prompt input
+    
+    if "system_prompt" not in st.session_state:
         file=open("Europai.txt",'r')
         context=file.read()
+        st.session_state["system_prompt"] = context
 
-        final_prompt=f"""utilises les consignes et informations du contexte pour répondre EN FRANCAIS a la demande de l'utilisateur:
-        
-        <context>
-            {context}
-        </context>
+    if "messages" not in st.session_state:
+      st.session_state.messages = []
 
-        question de l'utilisateur : {prompt}
-        
-        \n\n
-        répond en francais
-        \n\n
-        you use a lot of emoji in your answer
-        assistant:
+    # Add system prompt as a ChatMessage if it doesn't exist
+    if st.session_state["system_prompt"] and not any(message.role == "system" for message in st.session_state.messages):
+        st.session_state.messages.insert(0, ChatMessage(role="system", content=st.session_state["system_prompt"]))
 
-        """
-        response2 = llm.invoke(final_prompt)
-        
-        with st.chat_message(avatars["ai"]):
-            chat_box = st.empty()
-            chat_box.markdown(response2.content)
+    for message in st.session_state.messages:
+        if message.role != "system":  # Skip system messages for UI
+            with st.chat_message(avatars[message.role]):  # Use dot notation here
+                st.markdown(message.content)  # And here
 
-            msgs.add_ai_message(response2.content)
-        #sauvegarde l'état de la session historique du chat et param llm
-        
-    # Display the Groq logo
-    col, col1, col2, col3 = st.columns([1,1,1,1])
+    if prompt := st.chat_input("Quelle est votre question à propos des élections européenne?"): 
+        new_message = ChatMessage(role="user", content=prompt)
+        st.session_state.messages.append(new_message)
+        with st.chat_message(avatars["user"]):
+            st.markdown(prompt)
+
+        with st.chat_message(avatars["assistant"]):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in client.chat_stream(
+                model=st.session_state["mistral_model"],
+                temperature=0,
+                messages=st.session_state.messages,  # Pass the entire messages list
+            ):
+                full_response += (response.choices[0].delta.content or "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append(ChatMessage(role="assistant", content=full_response))
+    
+        # Display the Groq logo
+    col, col2, col3 = st.columns([1,1,1])
     with col:
         multidesc="""
-        <div style="background-color: rgb(14, 17, 23); color: white; font-size: 22px; display: inline-flex;">
+        <div style="background-color: rgb(14, 17, 23); color: white; font-size: 22px; display: inline-flex;"><a href="https://alpai.eu">
         <span style="color: #0091cc;">ALP</span><span style="color: #eb0a14;">AI</span>
         """
         st.markdown(multidesc, unsafe_allow_html=True)
         st.image('Alpai-logo-petit-large.png'); 
-        st.markdown("</div>",unsafe_allow_html=True)
-    with col1:
-        multidesc2="""
-        <div style="background-color: rgb(14, 17, 23); color: white; font-size: 22px; display: inline-flex;">
-        GROQ<span style="color: #f55036;">CLOUD</span>
-        """
-        st.markdown(multidesc2, unsafe_allow_html=True)
-        st.image('groqcloud_darkmode90.png')
-        st.markdown("</div>",unsafe_allow_html=True)
+        st.markdown("</a></div>",unsafe_allow_html=True)
 
     with col2:
         multidesc3="""
-        <div style="background-color: rgb(14, 17, 23); color: white; font-size: 22px; display: inline-flex;">
-        stream<span style="color: #ff4b4b;">lit</span>
+        
         """
         st.markdown(multidesc3, unsafe_allow_html=True)
-        st.image('streamlit-logo.png')
-        st.markdown("</div>",unsafe_allow_html=True)
+        
     with col3:
         multidesc3="""
-        <div style="background-color: rgb(14, 17, 23); color: white; font-size: 22px; display: inline-flex;">
+        <div style="background-color: rgb(14, 17, 23); color: white; font-size: 22px; display: inline-flex;"><a href="https://mistral.ai">
         Mist<span style="color: #fe4900;">ralAI</span>
         """
         st.markdown(multidesc3, unsafe_allow_html=True)
         st.image('mistralai-logo.png')
-        st.markdown("</div>",unsafe_allow_html=True)
-
+        st.markdown("</a></div>",unsafe_allow_html=True)
+    
+   
 
 if __name__ == "__main__":
     main()
